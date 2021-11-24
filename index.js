85862d9aac711cb90b0466c43efd0b753c637bf2
SHowardGal <u0250173@utah.edu>
2021-11-23 18:55:53 -0700

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+for (var x=o; x<=100; x++) {
+    if (x === 0) {
+        console.log(x + "is even");
+    }
+    else if (x % 2 === 0) {
+        console.log(x + "is even");
+    }
+    else {
+        console.log(x + "is odd");
+    }
+}
